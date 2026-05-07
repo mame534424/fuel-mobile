ac188d1cee31cd2d8eb88b3b4c2b127363063703
Mohammed Abrar <mame.534424@gmail.com>
2026-05-08 01:32:24 +0300

    chore: update dependencies and add new files for map and station details
    - Added new dependencies: @gorhom/bottom-sheet, @hookform/resolvers, babel-preset-expo, nativewind, prettier-plugin-tailwindcss, react-hook-form, zod, and tailwindcss.
    - Created new screen files: MapScreen.tsx and StationDetail.tsx.
    - Added api service file: api.ts.
    - Added Tailwind CSS configuration file: tailwind.config.js.
    - Updated tsconfig.json to include nativewind-env.d.ts.

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-3xl font-bold text-green-500">
+        E-Fuel Mobile App
+      </Text>
+    </View>
+  );
+}
