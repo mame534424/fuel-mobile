657417a764dea79b00c3927447052e7d2c2af3f7
Mohammed Abrar <mame.534424@gmail.com>
2026-05-17 22:17:40 +0300

    feat: update styling and dependencies for improved UI and performance

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
-
+import "../../global.css"
+import { Text, View } from "react-native";
+ 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-3xl font-bold text-green-500">
-        E-Fuel Mobile App
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
       </Text>
     </View>
   );
