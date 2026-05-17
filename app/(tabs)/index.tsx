f2d23b65b1e1a0cc2fb0413253169542413ab48c
Mohammed Abrar <mame.534424@gmail.com>
2026-05-17 22:20:13 +0300

    refactor: clean up and restore MapScreen component with theme support , final working version of native wind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,63 @@
 import "../../global.css"
-import { Text, View } from "react-native";
- 
-export default function App() {
+
+import { View, Text, Pressable }
+from "react-native";
+
+import { useThemeStore }
+from "@/stores/themeStore";
+
+export default function MapScreen() {
+
+  const {
+    theme,
+    toggleTheme,
+  } = useThemeStore();
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+    <View
+      className="
+        flex-1
+        items-center
+        justify-center
+        bg-white
+        dark:bg-black
+      "
+    >
+      <Text
+        className="
+          text-3xl
+          font-bold
+          text-green-700
+          dark:text-white
+        "
+      >
+        E-Fuel
       </Text>
+
+      <Text
+        className="
+          mt-3
+          text-black
+          dark:text-white
+        "
+      >
+        Current Theme: {theme}
+      </Text>
+
+      <Pressable
+        onPress={toggleTheme}
+        className="
+          mt-6
+          rounded-2xl
+          bg-green-700
+          px-6
+          py-4
+        "
+      >
+        <Text className="text-white">
+          Toggle Theme
+        </Text>
+      </Pressable>
     </View>
   );
 }
-
-
-// import { View, Text, Pressable }
-// from "react-native";
-
-// import { useThemeStore }
-// from "@/stores/themeStore";
-
-// export default function MapScreen() {
-
-//   const {
-//     theme,
-//     toggleTheme,
-//   } = useThemeStore();
-
-//   return (
-//     <View
-//       className="
-//         flex-1
-//         items-center
-//         justify-center
-//         bg-black
-//         dark:bg-black
-//       "
-//     >
-//       <Text
-//         className="
-//           text-3xl
-//           font-bold
-//           text-green-700
-//           dark:text-white
-//         "
-//       >
-//         E-Fuel
-//       </Text>
-
-//       <Text
-//         className="
-//           mt-3
-//           text-black
-//           dark:text-white
-//         "
-//       >
-//         Current Theme: {theme}
-//       </Text>
-
-//       <Pressable
-//         onPress={toggleTheme}
-//         className="
-//           mt-6
-//           rounded-2xl
-//           bg-green-700
-//           px-6
-//           py-4
-//         "
-//       >
-//         <Text className="text-white">
-//           Toggle Theme
-//         </Text>
-//       </Pressable>
-//     </View>
-//   );
-// }
